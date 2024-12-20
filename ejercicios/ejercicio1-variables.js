8b0628d527020972eb8f7ead633910df09a21a86
pichu2707 <pichu_2707@hotmail.com>
2024-12-20 01:21:55 +0100

    Creando y subiendo ejercicios de JS

---
+++ ejercicios/ejercicio1-variables.js
@@ -0,0 +1,69 @@
+// 1. Escribe un comentario en una lÃ­nea.
+
+//Esto es el comentario en una línea
+
+// 2. Escribe un comentario en varias lÃ­neas.
+
+/**Esto es el comentario
+ * En dos líneas
+ */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos.
+
+let stringLet = "Hola mundo";
+let numberLet = 8;
+let booleanLet = true;
+let undefinedVarLet = undefined;
+let nullVarLet = null;
+
+// 4. Imprime por consola el valor de todas las variables.
+
+console.log(stringLet);
+console.log(numberLet);
+console.log(booleanLet);
+console.log(undefinedVarLet);
+console.log(nullVarLet);
+
+console.log(stringLet, numberLet, booleanLet, undefinedVarLet, nullVarLet);
+
+// 5. Imprime por consola el tipo de todas las variables.
+
+console.log(typeof stringLet);
+console.log(typeof numberLet);
+console.log(typeof booleanLet);
+console.log(typeof undefinedVarLet);
+console.log(typeof nullVarLet);
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo.
+
+stringLet = "Hola mundo, soy un string";
+numberLet = 10;    
+booleanLet = false;
+undefinedVarLet = "undefined";
+nullVarLet = "null";
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo.
+
+stringLet = 10;
+number = "Hola mundo, soy un string";
+boolean = 8;
+undefinedVar = null;
+nullVar = true;
+
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos.
+
+const stringConst = "Hola mundo";
+const numberConst = 8;
+const booleanConst = true;
+const undefinedConst = undefined;
+const nullConst = null;
+
+// 9. A continuación, modifica los valores de las constantes.
+// 10. Comenta las lÃ­neas que produzcan algÃºn tipo de error al ejecutarse.
+
+// stringConst = "Hola mundo, soy un string";
+// numberConst = 10;
+// booleanConst = false;
+// undefinedConst = "undefined";
+
